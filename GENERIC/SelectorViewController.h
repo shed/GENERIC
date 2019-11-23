618b648ec24b210a86ee006674d6442e176641f9
YFSTEINER <y.steiner@gmail.com>
2019-11-23 18:46:00 +0100

    first simple view (tableview) which will represent the list of classes

---
+++ GENERIC/SelectorViewController.h
@@ -0,0 +1,17 @@
+//
+//  SelectorViewController.h
+//  GENERIC
+//
+//  Created by shed on 23/11/2019.
+//  Copyright © 2019 shed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SelectorViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GENERIC/SelectorViewController.m
@@ -0,0 +1,32 @@
+//
+//  SelectorViewController.m
+//  GENERIC
+//
+//  Created by shed on 23/11/2019.
+//  Copyright © 2019 shed. All rights reserved.
+//
+
+#import "SelectorViewController.h"
+
+@interface SelectorViewController ()
+
+@end
+
+@implementation SelectorViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
